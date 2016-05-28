6f79c776ee4916dca16b15d7c7d49f03fc13d74d
Andrey96 <i@andrey96.ru>
2016-05-28 18:00:42 +0300

    Cleanup

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -31,8 +31,6 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/blog/index')),
 				array('label'=>'Create post', 'url'=>array('/blog/write'), 'visible'=>!Yii::app()->user->isGuest),
-				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
